dffe0d46cc1059643d05c2da611cbe5e2dbecd23
JosephKimiri <joseph.kimiri.macharia@gmail.com>
2022-12-08 09:05:48 +0300

    Remove Django Hardcoded SecretKey

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
+SECRET_KEY = 'lvb4i6jbfvz(27_5q6_#(5-j#nx()z%buhd@pr5rj^@%juh+av'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
